d76a3fd3c3434f6e30c93890641cdd483c5efdf2
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-03-16 07:55:06 +0600

    quiz container id problem slove

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
 };
@@ -57,7 +57,7 @@ const displayQuiz = (data) => {
     quizContainer.innerHTML = "";
     return;
   }
-
+  
   data.forEach((quiz, i) => {
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
@@ -74,6 +74,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
+
 document.querySelector("#submit").addEventlistener("click", () => {
   if (answers.length < 6) {
     return;
